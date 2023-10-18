f1a09c1efd08780ce310303ebabc6a3eb05ea599
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-10-17 22:46:23 -0300

    update: Exercicio_6.py and Exercicio_8.py

--- Exercicio_6.py
+++ Exercicio_6.py
@@ -13,9 +13,9 @@ ti = theta*((32+6*(theta/tau))/(13+8*(theta/tau)))
 td = theta*((4)/(11+2*(theta/tau)))
 
 #ajustes finos
-kp = kp - 2.77
-ti = ti + 6.57
-td = td + 0.174
+kp = kp - 2.0
+ti = ti + 6.5
+td = td - 0.15
 
 # Definição da Função de Transferência da Planta
 num = np.array([k])
@@ -54,9 +54,53 @@ Hcl = cnt.feedback(Hdel, 1)
 t = np.linspace(0, 30, 100)
 (t, y) = cnt.step_response(10*Hcl, t)
 
-plt.plot(t, y, label='Cohen & Coon')
+# plt.plot(t, y, label='Cohen & Coon Ajustado')
+# plt.xlabel('t [s]')
+# plt.ylabel('Amplitude')
+# plt.grid()
+# plt.legend()
+# plt.show()
+
+# Cálculo dos Parâmetros do Controlador PID usando as fórmulas CHR 2
+kp_chr = (0.95*tau)/(k*theta)
+ti_chr = 1.357*tau
+td_chr = 0.473*theta
+
+kp_chr -= 0.75
+ti_chr -= 3
+td_chr -= 0.23
+
+# Construção das Funções de Transferência para as partes P, I, e D usando CHR 2
+numkp_chr = np.array([kp_chr])
+denkp_chr = np.array([1])
+numki_chr = np.array([kp_chr])
+denki_chr = np.array([ti_chr, 0])
+numkd_chr = np.array([kp_chr*td_chr, 0])
+denkd_chr = np.array([1])
+Hkp_chr = cnt.tf(numkp_chr, denkp_chr)
+Hki_chr = cnt.tf(numki_chr, denki_chr)
+Hkd_chr = cnt.tf(numkd_chr, denkd_chr)
+
+# Combinação das partes P, I, e D para formar o Controlador PID completo (CHR 2)
+Hctrl1_chr = cnt.parallel(Hkp_chr, Hki_chr)
+Hctrl_chr = cnt.parallel(Hctrl1_chr, Hkd_chr)
+
+# Série da Planta com o Controlador PID (CHR 2)
+Hdel_chr = cnt.series(Hs, Hctrl_chr)
+
+# Construção do Sistema de Controle em Malha Fechada (CHR 2)
+Hcl_chr = cnt.feedback(Hdel_chr, 1)
+
+# Geração e Plotagem das Respostas ao Degrau dos Sistemas
+t = np.linspace(0, 30, 100)
+(t, y) = cnt.step_response(10*Hcl, t)
+(t_chr, y_chr) = cnt.step_response(10*Hcl_chr, t)
+
+plt.plot(t, y, label='Cohen & Coon Ajustado')
+plt.plot(t_chr, y_chr, linestyle='dashed', label='CHR 2 Ajustado')
 plt.xlabel('t [s]')
 plt.ylabel('Amplitude')
+plt.title('Comparação: Cohen & Coon vs CHR 2')
 plt.grid()
 plt.legend()
 plt.show()
--- Exercicio_8.py
+++ Exercicio_8.py
@@ -1,22 +1,65 @@
+import numpy as np
+import control as cnt
+import matplotlib.pyplot as plt
+
 def pid_calculator(theta, T, K, setpoint):
-    # Cálculos dos parâmetros PID
+    # Cálculos dos parâmetros PID pelo método de CHR
     kp_chr = 0.95 * T / (K * theta)
     ti_chr = 1.357 * T
     td_chr = 0.473 * theta
 
+    # Cálculos dos parâmetros PID pelo método de Cohen-Coon
     kp = ((1 / K) * (T / theta)) * ((4 / 3) + ((1 / 4) * (theta / T)))
     ti = theta * ((32 + (6 * (theta / T))) / (13 + (8 * (theta / T))))
     td = theta * (4 / (11 + 2 * (theta / T)))
 
-    print(f"\nResultados dos cálculos PID:")
-    print(f"{'Método':<25} {'Kp/Kc':<15} {'Ki/Ti':<15} {'Kd/Td':<15}")
-    print(f"{'-' * 60}")
-    print(f"{'PID do CHR':<25} {kp_chr:<15.5f} {ti_chr:<15.5f} {td_chr:<15.5f}")
-    print(f"{'PID do Cohen-Coon':<25} {kp:<15.5f} {ti:<15.5f} {td:<15.5f}")
+    # Construindo as Funções de Transferência e a resposta ao degrau
 
+    # Planta
+    num = np.array([K])
+    den = np.array([T, 1])
+    H = cnt.tf(num, den)
+
+    # Aproximação de Pade para lidar com o atraso de tempo
+    n_pade = 20
+    (num_pade, den_pade) = cnt.pade(theta, n_pade)
+    H_pade = cnt.tf(num_pade, den_pade)
+    Hs = cnt.series(H, H_pade)
+
+    # Controlador Cohen-Coon
+    Hkp = cnt.tf([kp], [1])
+    Hki = cnt.tf([kp], [ti, 0])
+    Hkd = cnt.tf([kp*td, 0], [1])
+    Hctrl1 = cnt.parallel(Hkp, Hki)
+    Hctrl = cnt.parallel(Hctrl1, Hkd)
+    Hdel = cnt.series(Hs, Hctrl)
+    Hcl = cnt.feedback(Hdel, 1)
+
+    # Controlador CHR
+    Hkp_chr = cnt.tf([kp_chr], [1])
+    Hki_chr = cnt.tf([kp_chr], [ti_chr, 0])
+    Hkd_chr = cnt.tf([kp_chr*td_chr, 0], [1])
+    Hctrl1_chr = cnt.parallel(Hkp_chr, Hki_chr)
+    Hctrl_chr = cnt.parallel(Hctrl1_chr, Hkd_chr)
+    Hdel_chr = cnt.series(Hs, Hctrl_chr)
+    Hcl_chr = cnt.feedback(Hdel_chr, 1)
+
+    # Plotagem das Respostas ao Degrau dos Sistemas
+    t = np.linspace(0, 30, 100)
+    (t, y) = cnt.step_response(10*Hcl, t)
+    (t_chr, y_chr) = cnt.step_response(10*Hcl_chr, t)
+
+    plt.plot(t, y, label='Cohen & Coon')
+    plt.plot(t_chr, y_chr, linestyle='dashed', label='CHR')
+    plt.xlabel('t [s]')
+    plt.ylabel('Amplitude')
+    plt.title('Comparação: Cohen & Coon vs CHR')
+    plt.grid()
+    plt.legend()
+    plt.show()
 
 def main():
-    print("Calculadora PID")
+    print("Calculadora PID e Comparação de Respostas")
 
     try:
         theta = float(input("Digite o valor de Theta (θ): "))
@@ -28,7 +71,6 @@ def main():
 
     except ValueError:
         print("\nPor favor, insira um número válido.")
-
 
 if __name__ == "__main__":
     main()
